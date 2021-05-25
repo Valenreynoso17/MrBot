26cfc25caaf875e1877a90f170f8b57e630de8f5
Valenreynoso17 <valenreynoso17@gmail.com>
2021-05-25 19:03:40 -0300

    Automatizacion Mr.Mine

---
+++ main.py
@@ -0,0 +1,38 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import win32api, win32con
+
+# pyautogui.displayMousePosition()
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    time.sleep(0.01)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+
+while True:
+   
+    # Cofres
+    if keyboard.is_pressed('space'):
+        time.sleep(0.05)
+        keyboard.press('shift')
+        for i in range(322,1360,115):
+            click(i, 640)
+        keyboard.release('shift')
+
+    # Pelea
+    if keyboard.is_pressed('z'):
+        for i in range(700,1001, 150):
+            for j in range(670, 791, 40):
+                click(i, j)
+
+    # Minerales
+    if keyboard.is_pressed('m'):
+        time.sleep(0.05)
+        for i in range(370,1291,115):
+            click(i, 640)
+
+    if keyboard.is_pressed('e'):
+        break
